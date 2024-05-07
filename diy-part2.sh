826e2f2eadb6b3a4fcb1f5b27f8599fdca3e8788
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-08 04:36:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 修改默认密码
-sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
+# sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
 
 # 修改主机名
 sed -i "s/ImmortalWrt/OpenWrt/g" ./package/base-files/files/bin/config_generate
@@ -16,7 +16,7 @@ sed -i 's/ImmortalWrt-5G/online/g' package/mtk/applications/mtwifi-cfg/files/mtw
 # 添加自定义插件
 git clone -b master https://github.com/vernesong/OpenClash.git package/feeds/luci-app-openclash
 
-# 科学 mate core------------------
+# openclash /Core /mate
 mkdir -p files/etc/openclash/core
 CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
 wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
