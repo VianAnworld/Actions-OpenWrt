c5a0889bdf3578beef3da229ddb7ae603405f3ed
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-08 02:24:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,7 +22,8 @@
 sed -i "s/ImmortalWrt/OpenWrt/g" ./package/base-files/files/bin/config_generate
 ##-----------------修改 Lan IP ------------------
 sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generate
-
+##-----------------自定义插件------------------
+git clone -b master https://github.com/vernesong/OpenClash.git package/lean/luci-app-openclash
 ##-----------------Add OpenClash dev core------------------
 curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/dev/clash-linux-arm64.tar.gz -o /tmp/clash-linux-arm64.tar.gz
 tar zxvf /tmp/clash-linux-arm64.tar.gz -C /tmp >/dev/null 2>&1
