0299d15d52a3d115b1edcce90a9fdb9125fdd567
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-08 02:18:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,5 +18,15 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+##-----------------修改主机名------------------
 sed -i "s/ImmortalWrt/OpenWrt/g" ./package/base-files/files/bin/config_generate
+##-----------------修改 Lan IP ------------------
 sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+
+##-----------------Add OpenClash dev core------------------
+curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/dev/clash-linux-arm64.tar.gz -o /tmp/clash-linux-arm64.tar.gz
+tar zxvf /tmp/clash-linux-arm64.tar.gz -C /tmp >/dev/null 2>&1
+chmod +x /tmp/clash >/dev/null 2>&1
+mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
+mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
+rm -rf /tmp/clash-linux-arm64.tar.gz >/dev/null 2>&1
