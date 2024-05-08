8f04f95754c7029aa257c481a093f7849f19bb6f
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-08 20:54:39 +0800

    Delete preset-clash-core.sh

--- preset-clash-core.sh
+++
@@ -1,8 +0,0 @@
-echo -e "预置Clash内核"
-mkdir -p package/luci-app-openclash/root/etc/openclash/core
-core_path="package/luci-app-openclash/root/etc/openclash/core"
-goe_path="package/luci-app-openclash/root/etc/openclash"
-
-CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
-wget -qO- $CLASH_META_URL | tar xOvz > $core_path/clash_meta
-chmod +x $core_path/clash*
