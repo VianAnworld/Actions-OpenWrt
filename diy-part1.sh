b5770d33450d85148819a19f898aba9a5aabca6b
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-09 03:53:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # openclash 科学上网
-git clone https://github.com/vernesong/OpenClash.git  -b master package/luci-app-openclash
+git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
