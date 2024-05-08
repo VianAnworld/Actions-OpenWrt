39f7478c6c0577d612050e5a571e6d46490ce689
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-08 14:23:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,3 +8,12 @@ sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generat
 sed -i 's/ImmortalWrt-2.4G/SmartHome/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 sed -i 's/ImmortalWrt-5G/online/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
+# openclash #Core
+cd openwrt/package
+git clone https://github.com/vernesong/OpenClash
+mv OpenClash/luci-app-openclash/ .
+rm -rf OpenClash/
+cd base-files/files
+mkdir -p etc/openclash/core && cd etc/openclash/core
+curl -L https://github.com/vernesong/OpenClash/blob/core/master/meta/clash-linux-arm64.tar.gz | tar zxf -
+chmod +x clash
