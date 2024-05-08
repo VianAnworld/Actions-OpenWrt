b18fb10054edfa5828f2fee2cfa634015fd82a06
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-08 18:38:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,13 +8,12 @@ sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generat
 sed -i 's/ImmortalWrt-2.4G/SmartHome/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 sed -i 's/ImmortalWrt-5G/online/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
-# openclash
+# 添加openclash
 git clone https://github.com/vernesong/OpenClash/tree/master package/luci-app-openclash
 # 加入OpenClash核心
 chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
 $GITHUB_WORKSPACE/preset-clash-core.sh
-#echo "
-# openclash
-#CONFIG_PACKAGE_luci-app-openclash=y
+echo "
+CONFIG_PACKAGE_luci-app-openclash=y
 
-#" >> ax6000.config
+" >> ax6000.config
