787b580e0299359019bb8570b628196aae2806e0
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-10 02:23:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -i '$a src-git openwrt_kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git openwrt_kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
