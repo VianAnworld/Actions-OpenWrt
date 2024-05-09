65df89fba2cd0dafab65aac764c462788e7f09b7
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-10 04:16:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,3 +4,6 @@ sed -i "s/ImmortalWrt/OpenWrt/g" ./package/base-files/files/bin/config_generate
 sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt-2.4G/SmartHome/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 sed -i 's/ImmortalWrt-5G/online/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+
+# openclash
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash
