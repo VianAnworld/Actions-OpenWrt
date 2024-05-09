27ab1aa04b4c7a802908a6264afd760ea6e1b821
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-10 04:15:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 #sed -i '$a src-git openwrt_kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash
