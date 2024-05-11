008cca438c02315e2e6236e00720e06d8f1a473a
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-12 03:39:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # openclash
-#git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+find ./ | grep Makefile | xargs rm -f
+git clone https://github.com/vernesong/OpenClash.git -b master package/luci-app-openclash
+#echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 # passwall
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
