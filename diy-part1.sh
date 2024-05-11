b2981e09e3a6b87944c5e76a086fa92190fcc62f
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-11 18:32:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Add a feed source
-echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
