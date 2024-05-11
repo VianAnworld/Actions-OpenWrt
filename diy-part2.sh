d625374a4d62bc48f37c88b8cb8afa2271e04de6
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-11 18:37:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-## 主机名
+# 主机名
 sed -i "s/ImmortalWrt/OpenWrt/g" ./package/base-files/files/bin/config_generate
-## Modify default IP
+# Modify default IP
 sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generate
-## Modify wifi name
+# Modify wifi name
 sed -i 's/ImmortalWrt-2.4G/SmartHome/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 sed -i 's/ImmortalWrt-5G/online/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
-## Add OpenClash dev core
+# OpenClash dev core
 curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
 tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
 chmod +x /tmp/clash >/dev/null 2>&1
