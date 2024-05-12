c850744b29398a49e3e8dea181948dcd64f86d8f
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-13 04:05:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Modify主机名
+# Modify system hostname
 sed -i "s/ImmortalWrt/OpenWrt/g" ./package/base-files/files/bin/config_generate
 # Modify default IP
 sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generate
