fc5cbaad188c68653d4768e99a61062243b30d54
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-12 15:28:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,7 @@ sed -i 's/ImmortalWrt-2.4G/SmartHome/g' package/mtk/applications/mtwifi-cfg/file
 sed -i 's/ImmortalWrt-5G/online/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 # 删除bootstrap主题
 rm -rf feeds/luci/themes/luci-theme-bootstrap
+rm -rf feeds/luci/applications/luci-app-openclash
 # OpenClash dev core
 curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
 tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
