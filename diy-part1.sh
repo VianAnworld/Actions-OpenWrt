6036653afc9541d76b5c84c11dcea4a2f2b972b1
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-14 02:56:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
-#删除bootstrap主题
-rm -rf luci/themes/luci-theme-bootstrap-mod
-rm -rf luci/themes/luci-theme-bootstrap
 # 删除重复包
 #rm -rf feeds/luci/applications/luci-app-openclash
-rm -rf luci/applications/luci-app-openclash
-git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+#rm -rf luci/applications/luci-app-openclash
+#git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
