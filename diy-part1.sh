87b578c5861f7769f099949f6672c6f1897e438b
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-14 03:25:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,4 +3,4 @@
 #rm -rf feeds/luci/applications/luci-app-openclash
 #rm -rf luci/applications/luci-app-openclash
 #git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-#sed -i '$a src-git-full luci https://github.com/immortalwrt/luci.git;openwrt-23.05' feeds.conf.default
+sed -i '$a src-git-full luci https://github.com/immortalwrt/luci.git;openwrt-23.05' feeds.conf.default
