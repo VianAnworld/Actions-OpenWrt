43895384fcfdb790b4e1060c82e7c9a5c99494fb
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-14 01:31:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Modify
-sed -i "s/ImmortalWrt/OpenWrt/g" ./package/base-files/files/bin/config_generate
+sed -i "s/ImmortalWrt/OpenWrt/g" /package/base-files/files/bin/config_generate
 sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt-2.4G/SmartHome/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 sed -i 's/ImmortalWrt-5G/online/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
