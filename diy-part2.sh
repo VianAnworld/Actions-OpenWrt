1f392c4e1dd16674adaacc258f927ac81197c11a
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-13 22:56:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# Modify system hostname
+# Modify
 sed -i "s/ImmortalWrt/OpenWrt/g" ./package/base-files/files/bin/config_generate
-
-# Modify default IP
 sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generate
-
-# Modify wifi name
 sed -i 's/ImmortalWrt-2.4G/SmartHome/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 sed -i 's/ImmortalWrt-5G/online/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+
+sed -i 's/root::0:0:99999:7:::/root:$1$mCAXgXUF$6bgDhPFZRbF.2w0zCTQw00:19856:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 #删除bootstrap主题
 #rm -rf feeds/luci/themes/luci-theme-bootstrap-mod
