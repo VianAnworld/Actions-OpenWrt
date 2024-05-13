c201ce31504daddc3fba48f52c60ad2038f278ea
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-14 03:43:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # Modify
 sed -i "s/ImmortalWrt/OpenWrt/g" /package/base-files/files/bin/config_generate
 sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generate
@@ -8,9 +6,9 @@ sed -i 's/ImmortalWrt-5G/online/g' package/mtk/applications/mtwifi-cfg/files/mtw
 sed -i 's/root::0:0:99999:7:::/root:$1$mCAXgXUF$6bgDhPFZRbF.2w0zCTQw00:19856:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # 删除重复包
-#rm -rf feeds/luci/applications/luci-app-openclash
-rm -rf luci/applications/luci-app-openclash
-git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-openclash
+#rm -rf luci/applications/luci-app-openclash
+git clone -b master https://github.com/vernesong/OpenClash.git feeds/luci/applications/luci-app-openclash
 # OpenClash dev core
 curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
 tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
