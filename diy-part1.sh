b43e3bd3261ea8955c4573f296255d6a09ecd0b4
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-14 02:53:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+
+#删除bootstrap主题
+rm -rf luci/themes/luci-theme-bootstrap-mod
+rm -rf luci/themes/luci-theme-bootstrap
+# 删除重复包
+#rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf luci/applications/luci-app-openclash
 # Add a feed source
 echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
