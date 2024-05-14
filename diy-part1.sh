ce117fd693a7c449c6517b34b37ca583b4ca391a
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-14 19:07:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #取消掉feeds.conf.default文件里面的luci的#注释
 sed -i 's/^#\(.*luci\)/\1/' feeds.conf.default
+src-git luci https://github.com/immortalwrt/luci.git;openwrt-23.05 feeds.conf.default
