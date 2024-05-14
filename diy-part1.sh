084130f8921f90b478530eeaac55d83aee3805c3
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-14 14:34:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,1 @@
 #!/bin/bash
-# 替换源
-sed -i 's/src-git-full luci https://github.com/immortalwrt/luci.git;openwrt-21.02/src-git-full luci https://github.com/immortalwrt/luci.git;openwrt-23.05/g' ./feeds.conf.default
