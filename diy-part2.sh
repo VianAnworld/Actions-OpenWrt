e37a7fa4ace450234f5efcb67a57805b3f6730d5
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-17 01:56:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,20 @@ sed -i 's/root::0:0:99999:7:::/root:$1$mCAXgXUF$6bgDhPFZRbF.2w0zCTQw00:19856:0:9
 
 # 替换源中openclash
 rm -rf luci/applications/luci-app-openclash
-git clone -b master https://github.com/vernesong/OpenClash.git luci/applications/luci-app-openclash
+# Clone 项目
+mkdir luci/applications/luci-app-openclash
+cd luci/applications/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull --depth 1 origin master
+git branch --set-upstream-to=origin/master master
+
+# 编译 po2lmo (如果有po2lmo可跳过)
+pushd luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
 
 # OpenClash dev core
 curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
