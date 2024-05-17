a6841ecede82240fd32ab180ef5e30420ebbbe8f
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-18 03:45:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #rm -rf ./feeds.conf.default
-#sed -i '$a src-git kenzok8 https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git kenzok8 https://github.com/kenzok8/small-package' feeds.conf.default
