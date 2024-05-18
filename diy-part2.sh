5f9c3be4b04027570c30d95cd07d8bf38f1f7f09
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-19 03:36:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,3 +8,5 @@ sed -i 's/ImmortalWrt-2.4G/SmartHome/g' ./package/mtk/applications/mtwifi-cfg/fi
 sed -i 's/ImmortalWrt-5G/online/g' ./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 # Modify root
 sed -i 's/root::0:0:99999:7:::/root:$1$mCAXgXUF$6bgDhPFZRbF.2w0zCTQw00:19856:0:99999:7:::/g' ./package/base-files/files/etc/shadow
+rm -rf feeds/luci/applications/luci-app-ssr-plus
+rm -rf feeds/packages/net/xray-core
