9185936a2a0377c3f087c2c480b27ee802f9e0d3
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-19 00:39:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,12 +11,29 @@ sed -i 's/ImmortalWrt-5G/online/g' ./package/mtk/applications/mtwifi-cfg/files/m
 sed -i 's/root::0:0:99999:7:::/root:$1$mCAXgXUF$6bgDhPFZRbF.2w0zCTQw00:19856:0:99999:7:::/g' ./package/base-files/files/etc/shadow
 # 替换源中ssrplus
 rm -rf feeds/luci/applications/luci-app-ssr-plus
-rm -rf feeds/packages/net/xray-core
-rm -rf feeds/packages/net/xray-plugin
 rm -rf feeds/packages/net/chinadns-ng
+rm -rf feeds/packages/net/dns2socks
+rm -rf feeds/packages/net/dns2tcp
+rm -rf feeds/packages/devel/gn
+rm -rf feeds/packages/net/hysteria
+rm -rf feeds/packages/net/ipt2socks
+rm -rf feeds/packages/net/lua-neturl
+rm -rf feeds/packages/net/microsocks
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/naiveproxy
+rm -rf feeds/packages/net/redsocks2
 rm -rf feeds/packages/net/shadowsocks-rust
 rm -rf feeds/packages/net/shadowsocksr-libev
-git clone -b master --filter=blob:none https://github.com/fw876/helloworld.git package/helloworld
+rm -rf feeds/packages/net/simple-obfs
+rm -rf feeds/packages/net/tcping
+rm -rf feeds/packages/net/trojan
+rm -rf feeds/packages/net/tuic-client
+rm -rf feeds/packages/net/v2ray-core
+rm -rf feeds/packages/net/v2ray-plugin
+rm -rf feeds/packages/net/v2raya
+rm -rf feeds/packages/net/xray-core
+rm -rf feeds/packages/net/xray-plugin
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 # 替换源中openclash
 #rm -rf feeds/luci/applications/luci-app-openclash
 #git clone -b master --filter=blob:none https://github.com/vernesong/OpenClash.git package/luci-app-openclash
