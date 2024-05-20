bb28d50dc3a1864194e3cedbb3b4401a2dad4314
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-20 17:55:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,5 +15,3 @@ rm -rf feeds/packages/net/dns2tcp
 rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-make menuconfig # choose LUCI -> Applications -> luci-app-mosdns
-make package/mosdns/luci-app-mosdns/compile V=s
