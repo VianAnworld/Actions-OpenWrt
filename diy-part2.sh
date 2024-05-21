fa741bd9ca3718254bf22f8c60240f3bfc2e0c53
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-22 02:40:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@ sed -i 's/192.168.6.1/192.168.1.1/g' ./package/base-files/files/bin/config_gener
 sed -i 's/ImmortalWrt-2.4G/SmartHome/g' ./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 sed -i 's/ImmortalWrt-5G/online/g' ./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
-# 修改root
+# 修改password
 sed -i 's/root::0:0:99999:7:::/root:$1$mCAXgXUF$6bgDhPFZRbF.2w0zCTQw00:19856:0:99999:7:::/g' ./package/base-files/files/etc/shadow
 
 # 修正CPU频率
