669046976187c8538faa12fe72d03ba12e8fcc38
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-26 04:07:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+# 更新Xray core
+wget -O feeds/packages/net/xray-core/Makefile https://raw.githubusercontent.com/immortalwrt/packages/master/net/xray-core/Makefile
+# 更新dns2tcp
+wget -O feeds/packages/net/xray-core/Makefile https://raw.githubusercontent.com/fw876/helloworld/master/dns2tcp/Makefile
