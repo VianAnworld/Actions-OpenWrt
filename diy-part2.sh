4725eb5113a90bbd6c49d13527035904b176d156
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-26 03:32:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,3 +19,5 @@ wget -O feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg 
 # 更新Xray core
 sed -i 's/1.8.3/1.8.13/g' feeds/packages/net/xray-core/Makefile
 sed -i 's/bdfa65c15cd25f931745d9c70c753503db5d119ff11960ca7b3a2e19c4b0a8d1/9e63fbeb4667c19e286389c370d30e9e904f4421784adcbe6cf4d6e172a2ac29/g' feeds/packages/net/xray-core/Makefile
+# 更新dns2tcp
+wget -O feeds/packages/net/xray-core/Makefile https://raw.githubusercontent.com/immortalwrt/packages/openwrt-21.02/net/dns2tcp/Makefile
