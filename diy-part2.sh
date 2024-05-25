d0286c0fad4ed72cb008b35c8a2ec4952123dda0
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-26 05:46:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,12 +17,28 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 # 更换主题背景
 wget -O feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg https://raw.githubusercontent.com/VianAnworld/Actions-OpenWrt/main/1.jpg
 
-# 更新 Xray-core
-rm -rf feeds/packages/net/xray-core
-git clone -b main https://github.com/rufengsuixing/luci-app-onliner.git feeds/packages/net/xray-core
-# 更新 dns2tcp
-rm -rf feeds/packages/net/dns2tcp
-git clone -b master https://github.com/zfl9/dns2tcp.git feeds/packages/net/dns2tcp
-# 更新 chinadns-ng
+# 更新 ssrplus
 rm -rf feeds/packages/net/chinadns-ng
-git clone -b master https://github.com/zfl9/chinadns-ng.git feeds/packages/net/chinadns-ng
+rm -rf feeds/packages/net/dns2socks
+rm -rf feeds/packages/net/dns2tcp
+rm -rf feeds/packages/devel/gn
+rm -rf feeds/packages/net/hysteria
+rm -rf feeds/packages/net/ipt2socks
+rm -rf feeds/packages/lang/lua-neturl
+rm -rf feeds/luci/applications/luci-app-ssr-plus
+rm -rf feeds/packages/net/microsocks
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/naiveproxy
+rm -rf feeds/packages/net/redsocks2
+rm -rf feeds/packages/net/shadowsocks-rust
+rm -rf feeds/packages/net/shadowsocksr-libev
+rm -rf feeds/packages/net/simple-obfs
+rm -rf feeds/packages/net/tcping
+rm -rf feeds/packages/net/trojan
+rm -rf feeds/packages/net/tuic-client
+rm -rf feeds/packages/net/v2ray-core
+rm -rf feeds/packages/net/v2ray-plugin
+rm -rf feeds/packages/net/v2raya
+rm -rf feeds/packages/net/xray-core
+rm -rf feeds/packages/net/xray-plugin
+git clone --depth=1 https://github.com/fw876/helloworld.git feeds/package/helloworld
