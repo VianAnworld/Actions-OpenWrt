47408547c475addee55540e4f6a09ad73a5fbf34
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-26 16:51:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,4 +21,5 @@ rm -rf feeds/packages/lang/golang
 git clone --depth=1 https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
 # 更新 xray-core
 #rm -rf feeds/packages/net/xray-core
-#git clone --depth=1 https://github.com/XTLS/Xray-core.git feeds/packages/net/xray-core
+sed -i 's/1.8.3/1.8.13/g' feeds/packages/net/xray-core/Makefile
+sed -i 's/bdfa65c15cd25f931745d9c70c753503db5d119ff11960ca7b3a2e19c4b0a8d1/9e63fbeb4667c19e286389c370d30e9e904f4421784adcbe6cf4d6e172a2ac29/g' feeds/packages/net/xray-core/Makefile
