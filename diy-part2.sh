6405116c5b253ecf56c57d443f11b6cf42ae5389
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-05-31 02:33:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@ sed -i "s/DISTRIB_REVISION=.*/DISTRIB_REVISION='ImmortalWrt-21.02'/g" ./package/
 # 设置dns缓存为0
 sed -i 's/8000/0/g' ./package/network/services/dnsmasq/files/dhcp.conf
 # 禁止解析 IPv6 DNS 记录
-sed -i 's/option filter_aaaa	0/option filter_aaaa	1/g' ./package/network/services/dnsmasq/files/dhcp.conf
+sed -i 's/aaaa	0/aaaa	1/g' ./package/network/services/dnsmasq/files/dhcp.conf
 # 修改登录IP
 sed -i 's/192.168.6.1/192.168.1.1/g' ./package/base-files/files/bin/config_generate
 # 修改password
