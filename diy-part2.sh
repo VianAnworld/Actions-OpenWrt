01d9e3ab65406f695702c288edd12b0ec8b9819f
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-06-01 23:28:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -38,4 +38,9 @@ mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
 rm -rf /tmp/clash-meta.tar.gz >/dev/null 2>&1
 
 
-git clone https://github.com/muink/luci-app-homeproxy.git package/luci-app-homeproxy
+
+./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
