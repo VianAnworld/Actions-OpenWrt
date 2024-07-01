f8319b359429b028b840b9120e5b26b063cf0dca
Vian An <146716492+VianAnworld@users.noreply.github.com>
2024-07-02 04:42:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,30 +22,10 @@ sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 wget -O feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg https://raw.githubusercontent.com/VianAnworld/Actions-OpenWrt/main/1.jpg
 
-
-# 更新 golang
-#rm -rf feeds/packages/lang/golang
-#git clone --depth=1 -b 21.x  https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
-
-# 更新openclash
- mkdir package/luci-app-openclash
- cd package/luci-app-openclash
- git init
- git remote add -f origin https://github.com/vernesong/OpenClash.git
- git config core.sparsecheckout true
- echo "luci-app-openclash" >> .git/info/sparse-checkout
- git pull --depth 1 origin master
- git branch --set-upstream-to=origin/master master
-
- # 编译 po2lmo (如果有po2lmo可跳过)
- pushd luci-app-openclash/tools/po2lmo
- make && sudo make install
- popd
-
 # 添加 OpenClash Meta 内核
 curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz -o /tmp/clash-meta.tar.gz
 tar zxvf /tmp/clash-meta.tar.gz -C /tmp >/dev/null 2>&1
 chmod +x /tmp/clash >/dev/null 2>&1
-mkdir -p feeds/luci/applications/luci-app-openclash/luci-app-openclash/root/etc/openclash/core
-mv /tmp/clash feeds/luci/applications/luci-app-openclash/luci-app-openclash/root/etc/openclash/core/clash_meta >/dev/null 2>&1
+mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
+mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_meta >/dev/null 2>&1
 rm -rf /tmp/clash-meta.tar.gz >/dev/null 2>&1
